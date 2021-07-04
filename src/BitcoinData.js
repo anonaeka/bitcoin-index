84daa1920820c1a3f4a2f41cfe436175c1fb5617
anonaeka <anonvisualdesign@gmail.com>
2021-07-05 01:58:53 +1000

    first commit

---
+++ src/BitcoinData.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function BitcoinData() {
+    console [bitcoinData, setBitcoinData] = useState();
+
+    return (
+        <div>
+            {bitcoinData && ""}
+        </div>
+    )
+}
